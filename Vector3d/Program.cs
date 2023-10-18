2b509b3b34a216cbacaba3f5547d654ba1518106
TClaesborn <142004581+TClaesborn@users.noreply.github.com>
2023-10-18 10:35:41 +0200

    Add project files.

---
+++ Vector3d/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+class Program
+{
+
+    static void Main()
+    {
+        Vector3d v1 = new(3, 4, 0);
+        Console.WriteLine(v1);
+        Vector3d v0 = new();
+        Vector3d v2 = new(-1, 4, 2);
+        Console.WriteLine(v1.GetLength());
+        Console.WriteLine(v1.Length);
+        Vector3d v3 = new(v2);
+        Console.WriteLine( v3);
+        Vector3d v4 = Vector3d.Add(v1, v2);
+        Console.WriteLine(v4);
+        Vector3d v5 = v1 + v2;
+        Console.WriteLine(v5);
+        //Console.WriteLine(v5[7]);
+        v1.Normalize();
+        Console.WriteLine(v1);
+
+        double dot = v1 * v2;
+        Vector3d v6 = 3 * v1;
+        Console.WriteLine(v6);
+    }
+}
---
+++ Vector3d/Vector3d.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Vector3d
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+    public double Z { get; set; }
+
+    public double Length
+    {
+        get
+        {
+            return GetLength();
+        }
+    }
+
+    //Indexer
+    public double this[int i]
+    {
+        get
+        {
+            switch (i)
+            {
+                case 0:
+                    return X;
+                case 1:
+                    return Y;
+                case 2:
+                    return Z;
+                default:
+                    throw new IndexOutOfRangeException();
+            }
+        }
+    }
+
+    //Constructor (Overloads)
+    public Vector3d(double x, double y, double z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+    public Vector3d()
+    {
+        X = 0;
+        Y = 0;
+        Z = 0;
+    }
+    public Vector3d(Vector3d other)
+    {
+        X = other.X;
+        Y = other.Y;
+        Z = other.Z;
+    }
+
+    //Override (Polymorfi)
+    public override string ToString()
+    {
+        return $"[{X}, {Y}, {Z}]";
+    }
+
+    //Methods/Functions
+    public double GetLength()
+    {
+        return Math.Sqrt(X * X + Y * Y + Z * Z);
+    }
+
+    public static double Dot(Vector3d a, Vector3d b)
+    {
+        return a.X * b.X + a.Y * b.Y + a.Z * b.Z;
+    }
+
+    public void Normalize()
+    {
+        double len = Length;
+        X /= len;
+        Y /= len;
+        Z /= len;
+    }
+
+    public void Scale(double scaleFactor)
+    {
+        X *= scaleFactor;
+        Y *= scaleFactor;
+        Z *= scaleFactor;
+    }
+
+    public static Vector3d Add(Vector3d a, Vector3d b)
+    {
+        return new Vector3d(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+    }
+
+    //Operators (overload)
+    public static Vector3d operator +(Vector3d a, Vector3d b)
+    {
+        return Add(a, b);
+    }
+    public static double operator *(Vector3d a, Vector3d b)
+    {
+        return Dot(a, b);
+    }
+    public static Vector3d operator *(double a, Vector3d b)
+    {
+        return new Vector3d(a*b.X, a*b.Y, a*b.Z);
+    }
+}
+
